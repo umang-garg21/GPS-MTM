61e162f8a9d6f30be2e9645bdac8365386c8d4c9
Umang <umang@ucsb.edu>
2025-02-03 13:51:53 -0800

    modified train_examples.sh

--- train_examples.sh
+++ train_examples.sh
@@ -1,6 +1,8 @@
 # ***************************
 # Running MTM
 # ***************************
+#$workspace_dir="/data/home/umang/Trajectory_project/mtm"
+
 # MTM on D4RL Hoppper Medium-Replay
 python research/mtm/train.py +exp_mtm=d4rl_cont wandb.project="11_mtm_d4rl_4_3" args.seed=0 dataset.env_name=hopper-medium-replay-v2 "args.mask_patterns=[AUTO_MASK]"
 
@@ -20,4 +22,4 @@ python research/mtm/train.py +exp_mtm=d4rl_cont dataset.train_val_split=0.01 dat
 python research/mtm/train.py +exp_exorl=exorl_cont dataset.env_name="point_mass_maze_reach_top_left" dataset.replay_buffer_dir="/data/home/umang/Trajectory_project/mtm/research/exorl/datasets/"
 
 # Running MTM on GPS trajectory data.
-python research/mtm/train.py +exp_traj=traj_cont ++datasets.env_name="gps_traj_masked_modelling" ++datasets.replay_buffer_dir="/data/home/umang/Trajectory_project/anomaly_traj_data/haystac_anomaly_data1/saved_agents_1000"
+python research/mtm/train.py +exp_traj=traj_cont ++datasets.env_name="gps_traj_masked_modelling" ++datasets.replay_buffer_dir="$workspace_dir/anomaly_traj_data/haystac_anomaly_data1/saved_agents_1000"
